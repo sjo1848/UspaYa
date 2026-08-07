07efb184834867b44a7fad559c4b410a6d49f59f
sjo1848 <34699048+sjo1848@users.noreply.github.com>
2026-08-07 14:21:10 -0300

    feat(api): register courier transit controller

--- apps/api/src/modules/delivery/delivery.module.ts
+++ apps/api/src/modules/delivery/delivery.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { CourierDeliveriesController } from './http/courier-deliveries.controller';
+import { CourierTransitController } from './http/courier-transit.controller';
 import { OperationsDeliveriesController } from './http/operations-deliveries.controller';
 
 @Module({
-  controllers: [OperationsDeliveriesController, CourierDeliveriesController],
+  controllers: [
+    OperationsDeliveriesController,
+    CourierDeliveriesController,
+    CourierTransitController,
+  ],
 })
 export class DeliveryModule {}
