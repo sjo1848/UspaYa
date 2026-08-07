85154934cd53533fdaa820874cb89a59d129eeab
sjo1848 <34699048+sjo1848@users.noreply.github.com>
2026-08-07 14:48:02 -0300

    feat(api): register final delivery controller

--- apps/api/src/modules/delivery/delivery.module.ts
+++ apps/api/src/modules/delivery/delivery.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { CourierDeliveriesController } from './http/courier-deliveries.controller';
+import { CourierFinalDeliveryController } from './http/courier-final-delivery.controller';
 import { CourierTransitController } from './http/courier-transit.controller';
 import { OperationsDeliveriesController } from './http/operations-deliveries.controller';
 
@@ -9,6 +10,7 @@ import { OperationsDeliveriesController } from './http/operations-deliveries.con
     OperationsDeliveriesController,
     CourierDeliveriesController,
     CourierTransitController,
+    CourierFinalDeliveryController,
   ],
 })
 export class DeliveryModule {}
