dd938559bc39823c9caa2422509ff7b246d7cd9f
sjo1848 <34699048+sjo1848@users.noreply.github.com>
2026-08-07 01:14:18 -0300

    feat(api): register identity module

---
+++ apps/api/src/modules/identity/identity.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+import { IdentityController } from './identity.controller';
+
+@Module({
+  controllers: [IdentityController],
+})
+export class IdentityModule {}
