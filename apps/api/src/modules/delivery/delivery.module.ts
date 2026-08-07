3ba6af2b2d4d53ce71eb9013a6b306b6b91948cb
sjo1848 <34699048+sjo1848@users.noreply.github.com>
2026-08-07 20:50:24 -0300

    feat(api): register operations courier read model

--- apps/api/src/modules/delivery/delivery.module.ts
+++ apps/api/src/modules/delivery/delivery.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { CourierDeliveriesController } from './http/courier-deliveries.controller';
 import { CourierFinalDeliveryController } from './http/courier-final-delivery.controller';
 import { CourierTransitController } from './http/courier-transit.controller';
+import { OperationsCouriersController } from './http/operations-couriers.controller';
 import { OperationsDeliveriesController } from './http/operations-deliveries.controller';
 
 @Module({
   controllers: [
     OperationsDeliveriesController,
+    OperationsCouriersController,
     CourierDeliveriesController,
     CourierTransitController,
     CourierFinalDeliveryController,
