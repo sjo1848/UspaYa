b4b45309efe81381a14342634562fc2db67fc823
sjo1848 <34699048+sjo1848@users.noreply.github.com>
2026-08-07 01:11:39 -0300

    feat(api): add Prisma application service

---
+++ apps/api/src/shared/database/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, type OnApplicationShutdown } from '@nestjs/common';
+import {
+  closePrismaClient,
+  getPrismaClient,
+  type PrismaClient,
+} from '@uspaya/database';
+
+@Injectable()
+export class PrismaService implements OnApplicationShutdown {
+  readonly client: PrismaClient = getPrismaClient();
+
+  async onApplicationShutdown(): Promise<void> {
+    await closePrismaClient();
+  }
+}
